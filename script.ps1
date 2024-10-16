668820e3cae11196c9ddc1cbe3ed5b52f8741f94
Andriana <andri.ilkiv1@gmail.com>
2024-10-16 13:13:34 +0200

    Creacción del archivo

---
+++ script.ps1
@@ -0,0 +1,4 @@
+﻿New-Item -Path C:\directorio -ItemType Directory
+Set-Location -Path C:\directorio
+New-Item -Path C:\directorio\hola.txt -ItemType File | Out-File -FilePath C:\directorio\hola.txt -Encoding string -InputObject Andriana
+Copy-Item -Path C:\directorio\hola.txt -Destination C:\directorio\hola.bak
